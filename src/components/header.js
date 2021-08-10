b6905bfd110e68e431d70f48ecb2deadf508f416
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-11 03:11:53 +0700

    code header component

---
+++ src/components/header.js
@@ -0,0 +1,48 @@
+
+
+export default function Header () {
+  return (
+    <header className='header'>
+      <Container>
+        <TopBar>
+          <Logo />
+          <Nav />
+        </TopBar>
+      </Container>
+    </header>
+  );
+}
+
+function Container (props) {
+  return (
+    <div className="header__container">
+      {props.children}
+    </div>
+  )
+}
+
+function TopBar (props) {
+  return (
+    <div className="header__top-bar">
+      {props.children}
+    </div>
+  )
+}
+
+function Logo () {
+  return (
+    <span className="header__logo">
+      <img src='' />
+    </span>
+  )
+}
+
+function Nav () {
+  return (
+    <nav className="header__nav">
+      <div className="header__tab">
+        <div className="header__link"></div>
+      </div>
+    </nav>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
-
+//Importing the components
+import Header from "./header";
 
 export default function Layout () {
   return (
     <div className='layout'>
-      .
+      <Header />
     </div>
   );
 }
