58624bfe0b8e5ff23e1b892ba49c8c9164420938
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-12 02:50:26 +0700

    &_update

--- src/components/content.js
+++ src/components/content.js
@@ -4,26 +4,46 @@ import Pet1 from '../images/pet-1.png'
 export default function Content () {
   return (
     <section className="content">
-      <div className="content__our-story">
-        <div className="content__our-story-title">
-          OUR <p>Story</p>
-          <div className="content__our-story-pet">
-            <img src={Pet1} alt="Pet" />
-          </div>
-        </div>
-        <div className="content__our-story-text">
-          Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit,
-          sed do eiusmod tempor    incididunt ut labore et dolore magna aliqua.
-           Quis ipsum su spendisse ultrices gravida. Risus commodo viverra maec  enas accumsan
-            lacus vel facilisis.
-          <span>Read more</span>
-        </div>
-        <div className="content__our-story-food">
-          <img src={FoodIMG} alt="food" />
+      <OurStory />
+    </section>
+  )
+}
+
+function OurStory () {
+  let Title = () => {
+    return (
+      <div className="content__our-story-title">
+        OUR <p>Story</p>
+        <div className="content__our-story-pet">
+          <img src={Pet1} alt="Pet" />
         </div>
       </div>
-    </section>
+    )
+  }
+
+  let Text = () => {
+    return (
+      <div className="content__our-story-text">
+        Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit,
+        sed do eiusmod tempor    incididunt ut labore et dolore magna aliqua.
+        Quis ipsum su spendisse ultrices gravida. Risus commodo viverra maec  enas accumsan
+        lacus vel facilisis.
+        <span>Read more</span>
+      </div>
+    )
+  }
+
+  return (
+    <div className="content__our-story">
+      <Title />
+      
+      <Text />
+
+      <div className="content__our-story-food">
+        <img src={FoodIMG} alt="food" />
+      </div>
+    </div>
   )
 }
 
