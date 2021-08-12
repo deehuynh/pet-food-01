025b88b0e9938fb0e168ef5d18fa6f79e5ab33e1
Huynh Trung Nhan <maybemynameisxkd@gmail.com>
2021-08-12 17:43:10 +0700

    .

--- src/components/content.js
+++ src/components/content.js
@@ -1,6 +1,7 @@
 import FoodIMG from '../images/food-box.png'
 import Pet1 from '../images/pet-1.png'
 import Pet2 from '../images/pet-2.png'
+import Pet3 from '../images/pet-3.png'
 
 export default function Content () {
   return (
@@ -8,7 +9,53 @@ export default function Content () {
       <OurStory />
       <Ingredient />
       <FRow />
+      <SRow />
+      <TRow />
     </section>
+  )
+}
+
+function TRow () {
+  return (
+    <div className="content__trow"></div>
+  )
+}
+
+function SRow () {
+  return (
+    <div className="content__srow">
+      <Col addclass="content__col--srow1">
+        <ContentTitle addclass="content__title--srow">
+          Learn more about dog breeds
+        </ContentTitle>
+
+        <div className="content__search-bar">
+          <input type="text" placeholder="Search breeds" />
+        </div>
+
+        <ContentButton addclass="content__button--srow">
+          View all dog breeds
+        </ContentButton>
+      </Col>
+
+      <Col addclass="content__col--srow2">
+        <img src={Pet3} alt="pet" />
+      </Col>
+
+      <Col addclass="content__col--srow3">
+        <ContentTitle addclass="content__title--srow">
+          Thinking of getting a dog?
+        </ContentTitle>
+
+        <ContentText addclass="content__text--srow">
+          There are many things you need to know before getting a dog.
+        </ContentText>
+
+        <ContentButton addclass="content__button--srow">
+          Learn more
+        </ContentButton>
+      </Col>
+    </div>
   )
 }
 
@@ -16,18 +63,42 @@ function FRow () {
   return (
     <div className="content__frow">
       <Col addclass='content__col--frow1'>
-        <div className="content__title content__title--frow">The perfect start to life</div>
-        <div className="content__text content__text--frow">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        </div>
-        <span className="content__button">Learn more</span>
+        <ContentTitle addclass="content__title--frow">The perfect start to life</ContentTitle>
+        <ContentText  addclass="content__text--frow">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </ContentText>
+
+        <ContentButton addclass="content__button--frow">Learn more</ContentButton>
       </Col>
 
       <Col addclass='content__col--frow2'>
         <img src={Pet2} alt="Pet" />
       </Col>
     </div>
+  )
+}
+
+function ContentButton (props) {
+  const addclass = props.addclass ? props.addclass : '';
+  return (
+    <span className={`content__button ` + addclass}>{props.children}</span>
+  )
+}
+
+function ContentText (props) {
+  const addclass = props.addclass ? props.addclass : '';
+  return (
+    <div className={`content__text ` + addclass}>
+      {props.children}
+    </div>
+  )
+}
+
+function ContentTitle (props) {
+  const addclass = props.addclass ? props.addclass : '';
+  return (
+    <div className={`content__title ` + addclass}>{props.children}</div>
   )
 }
 
@@ -86,11 +157,5 @@ function OurStory () {
         <img src={FoodIMG} alt="food" />
       </div>
     </div>
-  )
-}
-
-function Button (props) {
-  return (
-    <span className="content__btn content__btn-our-story">{props.name}</span>
   )
 }
